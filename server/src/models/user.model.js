e35968e320d9f3f9eef601f86ccb12b10eb728cc
Nadav-Sonino <57793797+Nadav-Sonino@users.noreply.github.com>
2024-12-26 20:39:03 +0200

    Add user model

---
+++ server/src/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+          },
+          userName: {
+            type: String,
+            required: true,
+            unique: true,
+          },
+          password: {
+            type: String,
+            required: true,
+            minlength: 6,
+          },
+          profilePic: {
+            type: String,
+            default: "",
+          },
+    },
+    {timestamps: true}
+)
