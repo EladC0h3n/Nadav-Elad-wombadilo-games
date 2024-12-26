b99d6ff96f35431404f874aaacbb3721f283204e
Nadav-Sonino <57793797+Nadav-Sonino@users.noreply.github.com>
2024-12-26 17:21:05 +0200

    Init DB connection

---
+++ server/src/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import authRouter from './routes/auth.routes.js'
+import { connectDB } from './lib/db.js';
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5050;
+
+app.use(express.json());
+app.use(cookieParser())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth",authRouter);
+
+
+app.listen(PORT,() => {
+    console.log(`Listening on port ${PORT}`)
+    connectDB();
+});
