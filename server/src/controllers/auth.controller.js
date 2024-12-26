749231d9717c90b2c13b74c2eb4877c349ebc8e0
Nadav-Sonino <57793797+Nadav-Sonino@users.noreply.github.com>
2024-12-26 21:00:01 +0200

    Add signup logic

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,12 +1,89 @@
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import { generateToken } from "../lib/utils.js";
 
 
+export const signup = async (res,req) => {
+    const { userName, email, password } = req.body;
+    try {
+        if( !userName || !email || !password ){
+            res.status(400).json({ message: "All fields are required" });
+        }
+        if(password.length < 6){
+            res.status(400).json({ message: "Password must be at least 6 characters" });
+        }
 
-export const signup = async (res,req) => {};
+        const userByEmail = await User.findOne({ email });
+        if (userByEmail) {
+            res.status(400).json({ message: "Email already exists" });
+        }
 
-export const login = async (res,req) => {};
+        const userByUsername = await User.findOne({ userName });
+        if (userByUsername) {
+            res.status(400).json({ message: "UserName already exists" });
+        }
 
-export const logout = (res,req) => {};
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
-export const updateProfile = async (res,req) => {};
+        const newUser = new User({
+            email,
+            userName,
+            password: hashedPassword,
+        });
 
-export const checkAuth = (res,req) => {};
+        if(newUser){
+            generateToken(newUser._id, res);
+      
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            });
+        } else {
+            res.status(400).json({ message: "Invalid user data" });
+        }
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+export const login = async (res,req) => {
+    try {
+        
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+export const logout = (res,req) => {
+    try {
+        
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+export const updateProfile = async (res,req) => {
+    try {
+        
+    } catch (error) {
+        console.log("Error in updateProfile controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+export const checkAuth = (res,req) => {
+    try {
+        
+    } catch (error) {
+        console.log("Error in checkAuth controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { checkAuth, login, logout, signup } from '../controllers/auth.controller';
+import { protectRoute } from '../middleware/auth.middleware';
 const router = express.Router();
 
 
