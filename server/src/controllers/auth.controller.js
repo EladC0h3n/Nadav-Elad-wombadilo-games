9dacb7bf2ed9436bebbfc780b9ade87a92ceb0cf
Nadav-Sonino <57793797+Nadav-Sonino@users.noreply.github.com>
2024-12-27 16:10:02 +0200

    Add sendMessage logic

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
-import { generateToken } from "../lib/utils.js";
+import { generateToken, sendInternalError } from "../lib/utils.js";
 import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
--- server/src/controllers/messages.controller.js
+++ server/src/controllers/messages.controller.js
@@ -1,7 +1,8 @@
-import { sendInternalError } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
-import mongoose from "mongoose";
+import { sendInternalError } from "../lib/utils.js";
+import cloudinary from "../lib/cloudinary.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 
 export const getUsers = async (req, res) => {
     try {
@@ -17,13 +18,13 @@ export const getUsers = async (req, res) => {
 
 export const getMessages = async (req, res) => {
     try {
-        const { id: userToChatId } = req.params;
+        const { id: receiverId } = req.params;
         const loggedInUserId = req.user._id;
 
         const messages = await Message.find({
             $or: [
-                { senderId: loggedInUserId, receiverId: userToChatId },
-                { senderId: userToChatId, receiverId: loggedInUserId },
+                { senderId: loggedInUserId, receiverId: receiverId },
+                { senderId: receiverId, receiverId: loggedInUserId },
               ], 
         });
 
@@ -35,7 +36,34 @@ export const getMessages = async (req, res) => {
 
 export const sendMessage = async (req, res) => {
     try {
-        
+        const { text, image } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if (image) {
+            // Upload base64 image to cloudinary
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }else if(text.length <= 0) {
+            res.status(400).json({ message: "Please send text or/and image" })
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl,
+        });
+
+        await newMessage.save();
+
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
+
+        res.status(201).json(newMessage);
     } catch (error) {
         sendInternalError(error, res, "sendMessage");
     }
